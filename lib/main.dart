53d2211f9b34ca2b4d9643dc8b5035bb7b832991
UndertaleXFrisk <eng.vireaksathya@gmail.com>
2025-12-04 09:23:02 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'W8 - PRACTICE- Lists, Pass Functions/EXERCISE-1/ex_1_start.dart';
-import 'W8 - PRACTICE- Lists, Pass Functions/EXERCISE-2/main.dart';
-import 'W8 - PRACTICE- Lists, Pass Functions/EXERCISE-3/main.dart';
-import 'W8 - PRACTICE- Lists, Pass Functions/EXERCISE-4/main.dart';
+import 'package:flutter_template/quiz_app/ui/quiz_app.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    //home: Ex1(),
-    //home: Ex2(),
-    home: Ex3(),
-    //home: Ex4(),
-  ));
+  runApp(QuizApp());
 }
 
+class QuizApp extends MaterialApp {
+  const QuizApp({super.key}) : super(
+    title: 'Quiz App',
+    home: const QuizHome(),
+    debugShowCheckedModeBanner: false,
+  );
+}
