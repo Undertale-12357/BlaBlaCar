1a1c455e5c03a9168728aae01410282de6a258cf
Mek-Sans <eng.vireaksathya@gmail.com>
2025-12-28 20:33:38 +0700

    'update'

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/quiz_app/ui/quiz_app.dart';
+import 'package:flutter_template/expense_app/main.dart';
 
 void main() {
-  runApp(QuizApp());
-}
-
-class QuizApp extends MaterialApp {
-  const QuizApp({super.key}) : super(
-    title: 'Quiz App',
-    home: const QuizHome(),
-    debugShowCheckedModeBanner: false,
+  runApp(
+    App(),
   );
 }
